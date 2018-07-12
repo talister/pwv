f68751f6331b8d7bf02bff451f67d84da006e2ac
Tim Lister <tlister@lcogt.net>
2018-07-12 13:30:52 -0700

    Add routine to read and plot data from MERRA-2 NetCDF files

--- pwv/fetch_pwv.py
+++ pwv/fetch_pwv.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
+import os
 
 from urllib.parse import urljoin
 from astropy.table import QTable
 import astropy.units as u
+import netCDF4 as nc
 
 def convert_decimal_day(decimal_day):
 
@@ -47,3 +49,68 @@ def map_LCO_to_GPS_sites(site_code):
                 'OGG' : 'MAUI' }
 
     return mapping.get(site_code.upper(), None)
+
+def read_merra2(hdf_path, datafile, columns=['PS', 'T2M', 'QV2M', 'TO3', 'TQV']):
+
+    # Open file for reading, map into memory
+    data = nc.Dataset(os.path.join(hdf_path,datafile), mode='r', diskless=True)
+
+    # Extract longitude and latitude grid
+    lons = data.variables['lon'][:]
+    lats = data.variables['lat'][:]
+    table = { 'lons' : lons, 'lats' : lats }
+
+    # Read columns
+    for column in columns:
+        coldata = data.variables[column][:,:,:]
+        if column == 'TQV':
+             # (convert total preciptable water vapour in kg m^-2 to mm by dividing be density of water)
+            coldata = coldata/0.997
+            table['PWV'] = coldata
+        else:
+            table[column] = coldata
+
+    return table
+
+def plot_merra2_pwv(hdf_path, datafile):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+    from mpl_toolkits.basemap import Basemap
+
+    table = read_merra2(hdf_path, datafile, columns=['TQV'])
+
+    lons = table['lons']
+    lats = table['lats']
+    pwv = table['PWV']
+
+    if pwv.shape[0] > 1:
+        # Not a monthly mean, extract only 1st time slice
+        pwv = pwv[0,:,:]
+    plt.subplot(1,1,1)
+    map = Basemap(resolution='l', projection='eck4', lat_0=0, lon_0=0)
+    lon, lat = np.meshgrid(lons, lats)
+    xi, yi = map(lon, lat)
+    cs = map.pcolor(xi,yi,np.squeeze(pwv), vmin=np.min(pwv), vmax=np.max(pwv), cmap=cm.jet)
+    cs.set_edgecolor('face')
+    map.drawparallels(np.arange(-90., 90., 15.), labels=[1,0,0,0], fontsize=5)
+    map.drawmeridians(np.arange(-180., 180., 30.), labels=[0,0,0,1], fontsize=4)
+
+    # Add Coastlines, States, and Country Boundaries
+    map.drawcoastlines()
+    map.drawstates()
+    map.drawcountries()
+
+    cbar = map.colorbar(cs, location='bottom', pad="10%")
+    cbar.set_label('mm')
+    cbar.ax.tick_params(labelsize=10)
+
+    date_chunk = datafile.split('.')[2]
+    if date_chunk.isdigit() is False:
+        date_chunk = ''
+    plt.title('MERRA-2 PWV ({})'.format(date_chunk))
+    filename = 'MERRA2_PWV_{:s}_TEST.pdf'.format(date_chunk)
+    plt.savefig(filename, format='pdf', dpi=360)
+    plt.close()
+
+    return filename
